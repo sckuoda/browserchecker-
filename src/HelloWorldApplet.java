7e5b1dd9e6f62bca012ac2c8b630733801eea6d5
Samantha Kuo <samantha.kuo@gmail.com>
2016-01-17 15:03:13 -0500

    test applet commit

---
+++ src/HelloWorldApplet.java
@@ -0,0 +1,8 @@
+import java.applet.*;
+import java.awt.*;
+
+public class HelloWorldApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello world!", 50, 25);
+    }
+}
